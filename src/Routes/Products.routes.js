e581abaae4846271578fcf5043d724c8864175fd
Hernan Bueno <hbueno@teco.com.ar>
2022-08-11 17:15:03 -0300

    modificacion endpoint images

--- src/Routes/Products.routes.js
+++ src/Routes/Products.routes.js
@@ -1,12 +1,10 @@
 import express from 'express'
 import { getAllProducts,getProductsByID, putProducts, postProducts, deleteProducts } from '../Controllers/Products.controller.js'
 import{ verifyAdmin} from '../Middlewares/verifyAdmin.js'
-import {upload, uploadFile} from '../Utils/multer.js'
 export const Router = express.Router()
 .get('/', getAllProducts)
 .get('/:id', getProductsByID)
 //solo administrador
-.post("/upload", verifyAdmin, upload, uploadFile)
 .post('/',verifyAdmin, postProducts)
 .put('/:id',verifyAdmin, putProducts )
 .delete('/:id',verifyAdmin, deleteProducts)
--- src/Utils/multer.js
+++ src/Utils/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function(_req, _file, cb) {
-        cb(null, "uploads")
+        cb(null, "src/Uploads")
     },
     filename: function(_req, file, cb){
         cb(null, file.originalname )
@@ -12,7 +12,8 @@ const storage = multer.diskStorage({
 let upload = multer({ storage:storage })
 
 upload = upload.single("miFoto")
-function uploadFile(_req,res){
-    res.send({data:"El Archivo se subio correctamente"})
+function uploadFile(req,res){
+    const file = req.file
+    res.send({data:"El Archivo se subio correctamente", url:`${file.destination}/${file.filename}`})
 }
 export{upload, uploadFile}
